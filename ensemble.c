d45c56e4c030615ff532f9c027a666f6087e7c6e
criche <33120964+criche@users.noreply.github.com>
2018-03-22 15:30:47 +0100

    changement nom fichier

--- ensemble.c
+++ ensemble.c
@@ -1,5 +1,7 @@
 /*
-* fichier projet.c
+* fichier ensemble.c
 * Projet SDA2 printemps 2018
 * Chloé RICHE et Louis-César Pagès
 */
+
+#include "ensemble.h"
---
+++ ensemble.h
@@ -0,0 +1,32 @@
+/*
+* fichier ensemble.h
+* Projet SDA2 printemps 2018
+* Chloé RICHE et Louis-César Pagès
+*/
+
+#ifndef _EMS_H_
+#define _EMS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+typedef struct ensemble
+{
+  int* V;
+  size_t size;
+}  *Ens;
+
+void initOrderedSet();
+void freeOtderedSet();
+int getNumberElt(Ens e);
+int insertValue(Ens Str, int e);
+bool contains(Ens Str, int e);
+void printOrderedSet(Ens Str);
+int intersect(Ens Str, Ens Str2);
+
+
+#endif
