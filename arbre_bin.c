d4194e2d783f1ccfe24db9e9038c5fe721498c7d
criche <33120964+criche@users.noreply.github.com>
2018-03-22 16:22:39 +0100

    mise en forme des fichiers arbre_bin

---
+++ arbre_bin.c
@@ -0,0 +1,7 @@
+/*
+* fichier arbre_bin.c
+* Projet SDA2 printemps 2018
+* Chloé RICHE et Louis-César Pagès
+*/
+
+#include "arbre_bin.h"
---
+++ arbre_bin.h
@@ -0,0 +1,55 @@
+/*
+* fichier arbre_bin.h
+* Projet SDA2 printemps 2018
+* Chloé RICHE et Louis-César Pagès
+*/
+
+#ifndef _ABR_H_
+#define _ABR_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+/*
+* structure de l'arbre binaire de recherche
+* mot : mot du noeud
+* pos : ses positions dans le texte
+* gauche : branche gauche, mots avant dans le dico
+* droit : branche droite, mots après dans le dico
+*/
+typedef struct noeud
+{
+  char* mot;
+  Ens pos;
+  Node gauche;
+  Node droit;
+}  *Node;
+
+// création/destruction de l'ABR
+Node initBinarySearchTree();
+void freeBinarySearchTree(Node abr);
+
+// renvoie le nb de mots différents contenus dans l'ABR
+int getNumberString(Node abr);
+// renvoie le nb de paires mots-phrases différentes dans l'ABR
+int getTotalNumberString(Node abr);
+
+// insertion d'une nouvelle paire mot-position dans l'abr
+insert(Node abr, char* nouv_mot, int nouv_pos);
+
+// retrouve tous les indices d'occurrence du mot
+Ens find(Node abr, char* mot);
+// permet de retrouver tous les indices de co-occurrence des mots
+// 2 pour le moment
+// fonction variadique ?
+Ens findCooccurrences(Node abr, char* mot1, char* mot2);
+
+// affiche l'ABR
+// parcours en largeur ? (ordre alphabétique)
+void printBinarySearchTree(Node abr);
+
+#endif
