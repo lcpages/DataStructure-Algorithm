7cc366e524fc7dc55ba5ea7b42121839bb980ecf
criche <33120964+criche@users.noreply.github.com>
2018-03-22 15:14:54 +0100

    documents de base

---
+++ projet.c
@@ -0,0 +1,5 @@
+/*
+* fichier projet.c
+* Projet SDA2 printemps 2018
+* Chloé RICHE et Louis-César Pagès
+*/
---
+++ projet.h
@@ -0,0 +1,16 @@
+/*
+* fichier projet.h
+* Projet SDA2 printemps 2018
+* Chloé RICHE et Louis-César Pagès
+*/
+
+#ifndef _EMS_H_
+#define _EMS_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <inttypes.h>
+#include <time.h>
+#include <string.h>
+#include <stdbool.h>
+#include <unistd.h>
