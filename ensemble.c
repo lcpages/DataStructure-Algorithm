1c5398de69d4c450e597cd02aa85cbd3d017a9a6
criche <33120964+criche@users.noreply.github.com>
2018-04-07 13:52:56 +0200

    marche pas a regarder

--- ensemble.c
+++ ensemble.c
@@ -20,23 +20,64 @@ void freeOrderedSet(Ens st){
 }
 
 int getNumberElt(Ens st){
-
-        if(st->suiv == NULL) return 1;
-        return 1 + getNumberElt(st->suiv);
+  // si l'ensemble n'existe pas ou est vide
+  if(st == NULL || st->val == -1)
+  {
+    return 0;
+  }
+  else if(st->suiv == NULL) return 1;
+  return 1 + getNumberElt(st->suiv);
 }
 
 int insertValue(Ens st, int e){
+  // si on ne doit pas insérer l'élément
+  if(e < 0 || contains(st,e))
+  {
+    return 0;
+  }
+  // si l'ensemble est vide
+  else if(st->val == -1)
+  {
+    st->val = e;
+    return 1;
+  }
 
-        if(contains(st,e)){
-                return 0;
-        }
-        if(st->val == -1){
-            st->val =e;
-            return 1;
-        }
-       Ens sup = st;
+  Ens sup = st;
 
-       if(sup->val >e){
+  // si la valeur s'insère avant
+  if(sup->val > e)
+  {
+    Ens ex = malloc(sizeof(struct el));
+    ex->val = e;
+    ex->suiv = sup;
+    st = ex;
+    free(sup);
+    return 1;
+  }
+  else
+  {
+    // tant qu'on a pas atteint la fin de l'ensemble
+    while(sup->suiv != NULL)
+    {
+      // on doit insérer avant le suivant
+      if(sup->suiv->val > e)
+      {
+        Ens ex = malloc(sizeof(struct el));
+        ex->val = e;
+        ex->suiv = sup->suiv;
+        sup->suiv = ex;
+        return 1;
+      }
+      sup = sup->suiv;
+    }
+    // si on doit insérer au bout de l'ensemble
+    sup->val = e;
+    st->suiv = sup;
+    return 1;
+  }
+
+  return 0;
+    /*if(sup->val > e){
            Ens ex = malloc(sizeof(struct el));
            ex->suiv = sup->suiv;
            ex->val = sup->val;
@@ -72,8 +113,9 @@ int insertValue(Ens st, int e){
         // sup->suiv = initOrderedSet();
         //sup->suiv->val = e;
 
-        return 1;
+        return 1;*/
 }
+
 bool contains(Ens st, int e){
 
         if(st == NULL) return false;
--- main.c
+++ main.c
@@ -9,5 +9,15 @@
 
 int main()
 {
+  Ens a = initOrderedSet();
+  int nb_el = getNumberElt(a);
+  printf("Au départ, %d éléments dans a.\n", nb_el);
+
+  insertValue(a, 10);
+  insertValue(a, 200);
+  //insertValue(a, -1);
+  //insertValue(a, 178);
+  nb_el = getNumberElt(a);
+  printf("Après, %d éléments dans a.\n", nb_el);
   return 0;
 }
