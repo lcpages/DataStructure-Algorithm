0ec80bd5b28a75dd5b4d405c6994b298b4ff3c5d
criche <33120964+criche@users.noreply.github.com>
2018-04-27 16:59:00 +0200

    main

--- main.c
+++ main.c
@@ -9,36 +9,11 @@
 
 int main(int argc, char const *argv[])
 {
-
-    // Ens exemple = initOrderedSet();
-    // //Ens exemple2 =initOrderedSet();
-    // insertValue(exemple, 1);
-    // insertValue(exemple, 2);
-    // insertValue(exemple, 3);
-    // // insertValue(exemple2, 1);
-    // //
-    // // printf("Nombre éléments dans exemple : %d\n", getNumberElt(exemple));
-    // // printf("Nombre éléments dans exemple2 : %d\n",getNumberElt(exemple2));
-    // // //insertValue(exemple2, 1);
-    // //
-    //     printOrderedSet(exemple);
-    // printOrderedSet(exemple2);
-    // printf("\n" );
-    // Ens inter = intersect(exemple2, exemple);
-    // printOrderedSet(inter);
-    // printOrderedSet(exemple2);
-
-    Node t1 = initBinarySearchTree();
-    insert(t1,"toto", 2);
-    //printBinarySearchTree(t1);
-    insert(t1,"toto", 4);
-    insert(t1,"velo", 2);
-    insert(t1,"velo", 3);
-
-    insert(t1, "tata", 3);
-    insert(t1, "tata", 2);
-    printBinarySearchTree(t1);
-    freeBinarySearchTree(t1);
+    Node t = createAuto("phrases.txt");
+    printf("%s\n", t->mot);
+    printOrderedSet(t->pos);
+    //printBinarySearchTree(t);
+    freeBinarySearchTree(t);
 
     return 0;
 }
